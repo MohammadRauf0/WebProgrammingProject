9341392a9f20e31854b22e62049e43f15f68997b
godofcode007 <tariq_muzamil@live.com>
2023-12-19 17:57:40 +0300

    Dashboard component updated

--- Dashboard/navbarBoot.php
+++ Dashboard/navbarBoot.php
@@ -110,6 +110,7 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
+    <!-- the reason for having a min in the end of url is so that we get exactly what we need and not everything -->
 
 
 </body>
