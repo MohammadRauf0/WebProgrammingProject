3daf2b0f18546c68b6f4600969c7e4ca1381cb0e
Cactuskiller <daniaayad728@gamil.com>
2023-12-31 16:30:48 +0300

    remade login and sign up with bootstrap

---
+++ Authontication/createTableA.php
@@ -0,0 +1,30 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "notables";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE TABLE User (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_pass VARCHAR(255) NOT NULL
+ )";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table Notes created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
