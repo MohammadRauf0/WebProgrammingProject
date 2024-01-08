08c560e2cb0f4b051253538a914def5186a86566
godofcode007 <tariq_muzamil@live.com>
2024-01-08 17:09:07 +0300

    Refactor user profile update logic and SQL queries

--- profile.php
+++ profile.php
@@ -4,42 +4,60 @@ session_start();
 require 'config.php';
 require "message.php";
 
+// retrive data from the database everything about current user except the password
+if (!isset($_SESSION['userId'])) {
+  // Redirect to login page or handle not being logged in
+  exit('User is not logged in.');
+}
 
 $user_id = $_SESSION['userId'];
-$select = "SELECT * FROM user WHERE id = '$user_id'";
-$runsel = mysqli_query($con, $select);
-//testing 
-if (!$runsel) {
+$select = "SELECT id, user_name, first_name, last_name, user_email FROM user WHERE id = ?";
+$stmt = mysqli_prepare($con, $select);
+mysqli_stmt_bind_param($stmt, 'i', $user_id);
+mysqli_stmt_execute($stmt);
+$resultUsers = mysqli_stmt_get_result($stmt);
+
+if (!$resultUsers) {
   die('Error in SQL query: ' . mysqli_error($con));
 }
 
-@$post1 = $_POST['get1']; //username
-@$post2 = $_POST['get2']; //firstname
-@$post3 = $_POST['get3']; //lastname
-@$post4 = $_POST['get4']; //email
-@$post5 = $_POST['get5']; //password
-@$post6 = $_POST['get6']; //confirm password
+$rowselect = mysqli_fetch_array($resultUsers);
 
-$rowselect = mysqli_fetch_array($runsel);
+// Update the user profile
+
 if (isset($_POST['edit'])) {
-  // Hash the password
-  $hashedPassword = password_hash($post5, PASSWORD_DEFAULT);
+  $username = $_POST['user_name'];
+  $firstname = $_POST['first_name'];
+  $lastname = $_POST['last_name'];
+  $email = $_POST['user_email'];
+  $newPassword = $_POST['new_pass'];
 
-  $upUser = "UPDATE user SET 
-    user_name ='$post1',
-    first_name ='$post2',
-    last_name = '$post3',
-    user_email ='$post4',
-    user_pass ='$hashedPassword',
-    conform_userP = '$post6'
-    WHERE id='$user_id' 
-  ";
+  // Hash the new password
+  $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 
-  if (mysqli_query($con, $upUser)) {
+  $updateQuery = "UPDATE user SET 
+            user_name = ?,
+            first_name = ?,
+            last_name = ?,
+            user_email = ?,
+            user_pass = ?,
+            conform_userP = ?
+            WHERE id = ? 
+        ";
+
+  $stmtUpdate = mysqli_prepare($con, $updateQuery);
+  mysqli_stmt_bind_param($stmtUpdate, 'ssssssi', $username, $firstname, $lastname, $email, $hashedPassword, $hashedPassword, $user_id);
+  $resultUpdate = mysqli_stmt_execute($stmtUpdate);
+
+  if ($resultUpdate) {
     $_SESSION['message'] = "Profile has been updated successfully!";
+  } else {
+    die('Error in SQL update query: ' . mysqli_error($con));
   }
 }
 ?>
+
+
 <div class="container mt-5">
   <div class="row">
     <div class="col d-flex align-items-center justify-content-center">
@@ -63,34 +81,28 @@ if (isset($_POST['edit'])) {
               <span class="input-group-text">
                 <i class="fa-solid fa-user"></i>
               </span>
-              <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username" value="<?php echo $rowselect['user_name']; ?>" name="get1" required />
+              <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username" value="<?php echo $rowselect['user_name']; ?>" name="user_name" required />
             </div>
             <div class="row g-3 mb-3">
               <div class="col">
-                <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="First Name" value="<?php echo $rowselect['first_name']; ?>" name="get2" required />
+                <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="First Name" value="<?php echo $rowselect['first_name']; ?>" name="first_name" required />
               </div>
               <div class="col">
-                <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Last Name" name="get3" value="<?php echo $rowselect['last_name']; ?>" required />
+                <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Last Name" name="last_name" value="<?php echo $rowselect['last_name']; ?>" required />
               </div>
             </div>
             <div class="input-group mb-3">
               <span class="input-group-text">
                 <i class="fa-solid fa-envelope"></i>
               </span>
-              <input type="email" class="form-control form-control-lg bg-light fs-6" placeholder="Email" name="get4" value="<?php echo $rowselect['user_email']; ?>" required />
+              <input type="email" class="form-control form-control-lg bg-light fs-6" placeholder="Email" name="user_email" value="<?php echo $rowselect['user_email']; ?>" required />
             </div>
 
             <div class="input-group mb-3">
               <span class="input-group-text">
                 <i class="fa-solid fa-key"></i>
               </span>
-              <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="password" name="get5" value="<?php echo ($rowselect['user_pass']); ?>" required />
-            </div>
-            <div class=" input-group mb-1 mb-3">
-              <span class="input-group-text">
-                <i class="fa-solid fa-key"></i>
-              </span>
-              <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Conform password" name="get6" value="<?php echo $rowselect['conform_userP']; ?>" required />
+              <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="New Password" name="new_pass" required />
             </div>
             <!-- end of the input felids -->
 
