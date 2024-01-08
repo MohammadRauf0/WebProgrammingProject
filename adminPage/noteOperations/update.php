f601d30654d194084186280f5e9a4a037b08b329
godofcode007 <tariq_muzamil@live.com>
2024-01-08 20:06:36 +0300

    Final Update for Web Programming Project!

--- adminPage/noteOperations/update.php
+++ adminPage/noteOperations/update.php
@@ -5,13 +5,12 @@ require '../config.php';
 
 $note_id = $_GET['id'];
 
-if (isset($_POST['update_user'])) {
+if (isset($_POST['update_note'])) { // Change 'update_user' to 'update_note'
 
   $id = mysqli_real_escape_string($con, $_POST['note_id']);
   $userId = mysqli_real_escape_string($con, $_POST['user_id']);
   $title = mysqli_real_escape_string($con, $_POST['title']);
   $content = mysqli_real_escape_string($con, $_POST['content']);
-
 
   $sql = "UPDATE notes SET 
             title = ?,
@@ -46,19 +45,17 @@ if (isset($_POST['update_user'])) {
         <div class="card-body">
 
           <?php
-
           if (isset($_GET['id'])) {
-            $note_id =$_GET['id'];
-            $sql = "SELECT * FROM user WHERE id = ?";
+            $note_id = $_GET['id'];
+            $sql = "SELECT * FROM notes WHERE id = ?";
             $stmt = $con->prepare($sql);
             $stmt->bind_param("i", $note_id);
             $stmt->execute();
             $result = $stmt->get_result();
 
-            // Fetch the user row
+            // Fetch the note row
             $notesRow = $result->fetch_assoc();
           }
-
           ?>
 
           <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?id=' . $_GET['id']); ?>" method="POST">
@@ -66,7 +63,7 @@ if (isset($_POST['update_user'])) {
             <div class="input-group mb-4">
               <span class="input-group-text">@</span>
               <div class="form-floating">
-                <input type="text" class="form-control" name="note_id" id="floatingInputGroup1" value="<?= $_GET['id']; ?>" placeholder="id" style="font-size: 20px" disabled>
+                <input type="text" class="form-control" name="note_id" id="floatingInputGroup1" value="<?= $_GET['id']; ?>" placeholder="id" style="font-size: 20px" readonly>
                 <label for="floatingInputGroup1">id</label>
               </div>
             </div>
@@ -74,10 +71,11 @@ if (isset($_POST['update_user'])) {
             <div class="input-group mb-4">
               <span class="input-group-text"><i class="fa-solid fa-user" style="font-size: 20px;"></i></span>
               <div class="form-floating">
-                <input type="text" class="form-control" name="user_id" id="floatingInputGroup1" placeholder="userId" value="<?= $notesRow['user_id']; ?>" style="font-size: 20px" disabled>
+                <input type="text" class="form-control" name="user_id" id="floatingInputGroup1" placeholder="userId" value="<?= $notesRow['user_id']; ?>" style="font-size: 20px" readonly>
                 <label for="floatingInputGroup1">userId</label>
               </div>
             </div>
+
 
 
             <div class="col input-group mb-4">
